40d6ce8911b7f26881f3286da8c07cc894f7d497
Yunus Aktas <punuky34@gmail.com>
2024-03-08 20:03:29 +0000

    Mac Changer

--- macchanger.py
+++ macchanger.py
@@ -3,7 +3,7 @@ import optparse
 def get_user_input():
     parse_object=optparse.OptionParser()
     parse_object.add_option("-i","--interface",dest="interface",help="interface to change")
-    parse_object.add_option("-m","--mac",dest="mac",help="new mac adresss.")
+    parse_object.add_option("-m","--mac",dest="mac",help="new mac adresss")
     return parse_object.parse_args()
 
 
