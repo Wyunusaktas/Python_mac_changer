806a8d36ee8f774f9d9df4651cb919839c55650a
Yunus Aktas <punuky34@gmail.com>
2024-03-08 20:02:18 +0000

    Mac changer v1

--- macchanger.py
+++ macchanger.py
@@ -3,7 +3,7 @@ import optparse
 def get_user_input():
     parse_object=optparse.OptionParser()
     parse_object.add_option("-i","--interface",dest="interface",help="interface to change")
-    parse_object.add_option("-m","--mac",dest="mac",help="new mac adresss")
+    parse_object.add_option("-m","--mac",dest="mac",help="new mac adresss.")
     return parse_object.parse_args()
 
 
@@ -11,7 +11,6 @@ def macchanger(user_interface,user_mac):
     subprocess.call(["ifconfig",user_interface,"down"])
     subprocess.call(["ifconfig",user_interface,"hw","ether",user_mac])
     subprocess.call(["ifconfig",user_interface,"up"])
-
 
 (get,arguments)=get_user_input()
 macchanger(get.interface,get.mac)
